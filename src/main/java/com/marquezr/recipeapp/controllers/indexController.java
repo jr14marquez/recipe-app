7f51042b7aa804158308ae8dce6c62d50e169375
JR Marquez <Jr14marquez@gmail.com>
2018-04-29 17:46:27 -0400

    add domain entities

---
+++ src/main/java/com/marquezr/recipeapp/controllers/indexController.java
@@ -0,0 +1,14 @@
+package com.marquezr.recipeapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class indexController {
+
+    @RequestMapping({"","/","/index"})
+    public String getIndexPage(){
+        System.out.println("hello brahd");
+        return "index";
+    }
+}
