c5fe09678571ed9e0b7f5d3350c8f4f0b1ff42a3
jr14marquez <jr14marquez@gmail.com>
2018-05-01 08:58:04 -0600

    refactor using lombok

--- src/main/java/com/marquezr/recipeapp/controllers/indexController.java
+++ src/main/java/com/marquezr/recipeapp/controllers/indexController.java
@@ -1,10 +1,12 @@
 package com.marquezr.recipeapp.controllers;
 
 import com.marquezr.recipeapp.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Slf4j
 @Controller
 public class indexController {
 
